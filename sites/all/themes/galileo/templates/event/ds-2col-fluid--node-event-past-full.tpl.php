4efde14cb1231d49c1ea0c32a87b253c8ea27f98
Guicom <guicom.actency@gmail.Com>
2016-07-06 14:00:55 +0200

    Fix ortho

--- sites/all/themes/galileo/templates/event/ds-2col-fluid--node-event-past-full.tpl.php
+++ sites/all/themes/galileo/templates/event/ds-2col-fluid--node-event-past-full.tpl.php
@@ -1,7 +1,7 @@
 <div class="<?php print $classes;?> past-event clearfix">
   <div class="left-side col-md-8 col-xs-12">
     <div class="event-info col-xs-12">
-      <p class="date white">Cette soirée à eu lieu en <?php print $custom_date; ?></p>
+      <p class="date white">Cette soirée a eu lieu en <?php print $custom_date; ?></p>
     </div>
     <?php print render($content['body']); ?>
   </div>
